9dcb02ff4a0c2d5dc7876912409f903a929f3dba
milessmi <milessmi@usc.edu>
2026-04-16 19:29:36 -0700

    showing mike

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs'
 export default function SignInPage() {
   return (
     <div style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#F5F2EB' }}>
-      <SignIn />
+      <SignIn/>
     </div>
   )
 }
